0670c210fe91827fe3ce353c941ffa73c241d3da
Manikandan Ravi <manikandan.ravi@centrico.tech>
2020-09-18 14:56:34 +0530

    Modified controller with zip file download

--- src/main/java/com/mani/controller/EmployeeController.java
+++ src/main/java/com/mani/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.mani.controller;
 
 import com.mani.model.Employee;
+import com.mani.model.in.EmployeeRequest;
 import com.mani.persistence.EmployeeRepository;
 import com.mongodb.client.gridfs.GridFSFindIterable;
 import com.mongodb.client.gridfs.model.GridFSFile;
@@ -16,6 +17,7 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,29 +45,24 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Autowired
     private GridFsTemplate gridFsTemplate;
 
     @GetMapping(value = "/")
     public Employee getEmployee(@RequestParam  String name){
-       Employee employee =employeeRepository.findByName(name);
+        Employee employee =employeeRepository.findByName(name);
         System.out.println("Employee fetched successfully");
         return employee;
     }
 
     @PostMapping(value = "/")
-    public void getEmployee(){
-          Employee emp = new Employee("gbs05348","anbu","developer");
-           employeeRepository.save(emp);
-        Employee emp1 = new Employee("gbs05347","mani","developer");
-        employeeRepository.save(emp1);
+    public void getEmployee(@RequestBody EmployeeRequest employee){
+        Employee emp = new Employee(employee.getEmpId(),employee.getName(),employee.getDesignation());
+        employeeRepository.save(emp);
         System.out.println("insertion successfull");
     }
 
     @PostMapping("/uploadimage")
-    public String saveEmployeeFile(@RequestParam("name") String name, @RequestParam("imageFile") MultipartFile file){
+    public String saveEmployeeFile(@RequestParam("name") String name, @RequestParam("file") MultipartFile file){
         System.out.println(file.getOriginalFilename());
         try {
             Employee employee =employeeRepository.findByName(name);
@@ -82,10 +79,10 @@ public class EmployeeController {
     }
 
     @GetMapping("/image")
-    public String getEmployeeImage(HttpServletResponse response, String id){
-        GridFSFile file=gridFsTemplate.findOne(new Query(Criteria.where("metadata.name").is("mani").and("_id").is(id)));
+    public String getEmployeeImage(HttpServletResponse response, String id, String name){
+        GridFSFile file=gridFsTemplate.findOne(new Query(Criteria.where("metadata.name").is(name).and("_id").is(id)));
         try {
-          FileCopyUtils.copy( gridFsTemplate.getResource(file).getInputStream(), response.getOutputStream());
+            FileCopyUtils.copy( gridFsTemplate.getResource(file).getInputStream(), response.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -96,23 +93,22 @@ public class EmployeeController {
     public String getEmployeeImages(HttpServletResponse response, String name){
         response.setStatus(HttpServletResponse.SC_OK);
         response.addHeader("Content-Disposition", "attachment; filename=\"test.zip\"");
-        try(ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream())){
+
+        try(ZipOutputStream zipOutputStream= new ZipOutputStream(response.getOutputStream())){
+
             List<GridFSFile> fileList = new ArrayList<GridFSFile>();
             gridFsTemplate.find(new Query(Criteria.where("metadata.name").is(name))).into(fileList);
             //GridFSFindIterable iterable=gridFsTemplate.find(new Query(Criteria.where("metadata.name").is(name)));
             for (GridFSFile gridFSFile : fileList) {
-            //    InputStream inputStream = gridFsTemplate.getResource(gridFSFile).getInputStream();
+                //    InputStream inputStream = gridFsTemplate.getResource(gridFSFile).getInputStream();
                 zipOutputStream.putNextEntry(new ZipEntry(gridFSFile.getFilename()));
-                FileCopyUtils.copy(gridFsTemplate.getResource(gridFSFile).getInputStream(), zipOutputStream);
+                StreamUtils.copy(gridFsTemplate.getResource(gridFSFile).getInputStream(), zipOutputStream);
                 zipOutputStream.closeEntry();
-                zipOutputStream.finish();
             }
 
-            zipOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return "successfully downloaded";
     }
 
---
+++ src/main/java/com/mani/model/in/EmployeeRequest.java
@@ -0,0 +1,36 @@
+package com.mani.model.in;
+
+/**
+ * Created by gbs05347 on 18-09-2020.
+ */
+public class EmployeeRequest {
+    private String empId;
+
+    private String name;
+
+    private String designation;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+}
